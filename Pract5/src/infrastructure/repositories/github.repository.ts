b21bab917b424c3cfec8c4d7e97d089294987b8e
TONYMOR1208 <ajmc1316718111@gmail.com>
2024-06-14 23:56:09 -0500

    123

--- Pract5/src/infrastructure/repositories/github.repository.ts
+++
@@ -1,9 +0,0 @@
-import { GitHubDatasource } from '../datasource/github.datasource';
-
-export class GitHubRepository {
-  constructor(private readonly datasource: GitHubDatasource) {}
-
-  async getUserRepos(): Promise<any[]> {
-    return this.datasource.getUserRepos();
-  }
-}
