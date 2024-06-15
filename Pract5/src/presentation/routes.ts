a3bbcae909ffa5295ee8da06c4521b77cb20dbbe
TONYMOR1208 <ajmc1316718111@gmail.com>
2024-06-14 23:49:30 -0500

    commit2

--- Pract5/src/presentation/routes.ts
+++ Pract5/src/presentation/routes.ts
@@ -3,24 +3,16 @@ import { Router } from 'express';
 import {IdiomaRoutes} from './idiomas/routes';
 import { PalabraRoutes} from './palabras/routes';
 import {RegistroRoutes } from './registros/routes';
+import { GitHubRoutes } from './github/github.routes'; // Nueva ruta para GitHub
 
 
+const router = Router();
 
 
-export class AppRoutes {
+router.use('/api/idiomas', IdiomaRoutes.routes );
+router.use('/api/palabras', PalabraRoutes.routes );
+router.use('/api/registros', RegistroRoutes.routes );
+router.use('/github', GitHubRoutes.routes); // Ruta para GitHub
 
 
-  static get routes(): Router {
-
-    const router = Router();
-
-    router.use('/api/idiomas', IdiomaRoutes.routes );
-    router.use('/api/palabras', PalabraRoutes.routes );
-    router.use('/api/registros', RegistroRoutes.routes );
-
-    return router;
-  }
-
-
-}
-
+export const AppRoutes = { routes: router }; // Exporta las rutas de manera correcta
